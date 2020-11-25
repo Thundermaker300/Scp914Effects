d99e8c6a5f7e887d5ca9ae982638e49473676cc5
Thunder <Thundermaker300@gmail.com>
2020-11-25 11:52:48 -0500

    Version bump

--- Scp914Effects/Plugin.cs
+++ Scp914Effects/Plugin.cs
@@ -38,7 +38,7 @@ namespace Scp914Effects
 
         public override string Name => "Scp914Effects";
         public override string Author => "Thunder";
-        public override Version Version => new Version(1, 0, 0);
+        public override Version Version => new Version(1, 0, 1);
         public override Version RequiredExiledVersion => new Version(2, 1, 18);
         public override PluginPriority Priority => PluginPriority.High;
     }
